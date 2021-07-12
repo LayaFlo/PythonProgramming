1656c748e5a743e84da044e1152799464d2fb98e
LayaFlo <42862790+LayaFlo@users.noreply.github.com>
2021-07-12 17:43:47 +0300

    Update 5.1.ReadingFile.py

--- PythonProgramming/5.1.ReadingFile.py
+++ PythonProgramming/5.1.ReadingFile.py
@@ -1,3 +1,4 @@
+#Program which reads the entire content of the facts.txt file and prints it on the screen
 readfile = open("facts.txt")
 content = readfile.read()
 print("Following was read from the file:", content)
